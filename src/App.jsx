ff1598d3ed6ec435572245b0d1d7b71a8f0a703a
Rachit <rachitmishra23dec@gmail.com>
2026-04-11 18:38:25 +0530

    Template Integration 09-04-2026

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,8 @@
 import { useState } from 'react'
+export default function App(){
+    return(
+        <>
+        <h1>This is a app section</h1>
+        </>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import App from './App.jsx'
+import Navbar from './Components.jsx/Navbar.jsx'
+import Footer from './Components.jsx/Footer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Navbar />
+    <Footer />
   </StrictMode>,
 )
